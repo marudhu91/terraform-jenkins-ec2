6379e581bee2671c9eabdef791937e0f6d4f00f9
marudhu91 <72726492+marudhu91@users.noreply.github.com>
2020-10-13 10:16:14 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,16 +1,3 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_ami" "ec2_instance" {
-  most_recent = true
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["211050449168"]
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-098f16afa9edf40be"
   instance_type = "t2.micro"
